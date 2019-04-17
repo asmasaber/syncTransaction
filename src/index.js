82362c7d5c0b36aca6d0d4d5c9afccb35b4d4d81
asmasaber <asmaa.saber93@gmail.com>
2019-04-17 14:46:37 +0200

    check on promise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 import run from "./transaction"
+const fetch = require("node-fetch");
 
 function* generatorFunc() {
   const x = yield "1st";
   console.log("x >>", x);
-  const y = yield "2nd";
+  const y = yield fetch('https://jsonplaceholder.typicode.com/todos/1').then(response => response.json());
   console.log("y >>", y);
-  const z = yield "3rd";
+  const z = yield console.log("3rd");
   console.log("z >>", z);
   yield ;
 }
--- src/transaction/index.js
+++ src/transaction/index.js
@@ -1,4 +1,3 @@
-
 const run = (gen, ... args) => {
   let ltObj = gen( ... args);
   iterat(ltObj);
@@ -6,8 +5,17 @@ const run = (gen, ... args) => {
 
 const iterat = (obj, data) => {
   const result = obj.next(data);
-  if(!result.done)
-      iterat(obj, result.value)
+  if(!result.done) {
+    let arg = result.value ;
+      if(arg instanceof Promise) {
+        arg
+        .then(data => iterat(obj, data))
+        .catch(error => iterat(obj, error))
+      }
+      else {
+        iterat(obj, arg)
+      }
+  }
 }
-  
+
 export default run;
